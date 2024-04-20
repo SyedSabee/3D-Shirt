6eab54147c861ad06a772331d12e171175eea2b7
SyedSabee <sabishah352@gmail.com>
2024-04-21 04:19:40 +0500

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+import Filepicker from "./FilePicker";
+
+export { CustomButton, AIPicker, ColorPicker, Tab, Filepicker };
